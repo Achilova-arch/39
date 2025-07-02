865875895da9ffc079ab3488e2550c46333a9cbc
Achilova-arch <asalachilova96@gmail.com>
2025-07-02 15:28:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let ism=prompt("Ismiz")
+console.log("Sizning isingiz:" + ism);
+let yosh=prompt("Yoshiz")
+console.log("Sizning Yoshingiz:" + yosh);
+let hobbi=prompt("Siz yoqtiradigon hobbis")
+console.log("Sizning Hobbis:" + hobbi);
+let regiyon=prompt("Siz Yashoyotgan Viloyatiz")
+console.log("Sizning Yashoyotgan Viloyatiz:" + regiyon);
